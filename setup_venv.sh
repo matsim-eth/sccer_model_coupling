42612692eeee9bad5ffe3ebc6bea4af640233275
Christopher Tchervenkov <ctchervenkov@gmail.com>
2019-11-12 13:13:15 +0100

    updated requirements

--- setup_venv.sh
+++ setup_venv.sh
@@ -2,9 +2,9 @@
 
 module load new gcc/4.8.2 python/3.6.0
 
-python -m venv sccer-venv
+python3 -m venv sccer-venv
 . sccer-venv/bin/activate
-python --version
-which python
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python3 --version
+which python3
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
